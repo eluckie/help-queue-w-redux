e358a81f9d9aaf1d6d2c73deb99aaddb1e4fad90
E Luckie <eluckie.d@gmail.com>
2023-04-27 11:49:10 -0600

    add functionality to view time elapsed for each ticket

--- src/__tests__/reducers/ticket-list-reducer-test.js
+++ src/__tests__/reducers/ticket-list-reducer-test.js
@@ -1,5 +1,6 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 import * as c from "./../../actions/ActionTypes";
+import { format, formatDistanceToNow } from 'date-fns';
 
 describe('ticketListReducer', () => {
   let action;
@@ -22,6 +23,10 @@ describe('ticketListReducer', () => {
     names: "Ryan and Aimen",
     location: "4b",
     issue: "Redux action is not working correctly.",
+    timeOpen: new Date(),
+    formattedWaitTime: formatDistanceToNow(new Date(), {
+      addSuffix: true
+    }),
     id: 1
   };
 
@@ -29,13 +34,15 @@ describe('ticketListReducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
-  test('Should successfully add new ticket data to mainTicketList', () => {
-    const { names, location, issue, id } = ticketData;
+  test('Should successfully add a ticket to the ticket list that includes date-fns-formatted wait times', () => {
+    const { names, location, issue, timeOpen, formattedWaitTime, id } = ticketData;
     action = {
       type: c.ADD_TICKET,
       names: names,
       location: location,
       issue: issue,
+      timeOpen: timeOpen,
+      formattedWaitTime: formattedWaitTime,
       id: id
     };
 
@@ -44,6 +51,8 @@ describe('ticketListReducer', () => {
         names: names,
         location: location,
         issue: issue,
+        timeOpen: timeOpen,
+        formattedWaitTime: "less than a minute ago",
         id: id
       }
     });
@@ -60,6 +69,25 @@ describe('ticketListReducer', () => {
         location: '2a',
         issue: 'Reducer has side effects.',
         id: 2 
+      }
+    });
+  });
+
+  test("Should add a formatted wait time to ticket entry", () => {
+    const { names, location, issue, timeOpen, id } = ticketData;
+    action = {
+      type: c.UPDATE_TIME,
+      formattedWaitTime: "4 minutes ago",
+      id: id
+    };
+    expect(ticketListReducer({ [id]: ticketData }, action)).toEqual({
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: "4 minutes ago"
       }
     });
   });
